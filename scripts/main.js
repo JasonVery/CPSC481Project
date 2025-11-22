e868e8e15c1be0a5014320132fdd197d37f9fb90
JasonVery <jvery2001@gmail.com>
2025-11-22 03:24:30 -0700

    Better Scalable layout when we need to make changes. Still need to redo book information

---
+++ scripts/main.js
@@ -0,0 +1,124 @@
+
+// Main js file. Handles things like navigation and swithching pages
+// to make routing easier, everyhting is just in one html doc and just setting or removing
+// display to show different elements
+document.addEventListener("DOMContentLoaded", function () {
+
+    // Specific one off vars
+    const eyeCatcherHelpBtn = document.querySelector(".eyeCatcherHelpBtn");
+    const eyeCatcherStartBtn = document.querySelector(".eyeCatcherStartBtn");
+    const searchBar = document.querySelector(".searchBarCont");
+    const keyboard = document.getElementById("keyboardContainer");
+    let currentPage = "eye";
+
+
+    // Full pages -> These use Id attribute rather than class
+    const eyeCatcherPage = document.getElementById("eyeCatcherPage");
+    const mainPage = document.getElementById("mainPage");
+    const searchPage = document.getElementById("searchPage");
+    const resultsPage = document.getElementById("resultsPage");
+
+
+    // Navigation buttons
+    const backBtn = document.getElementById("backBtn");
+    const homeBtn = document.getElementById("homeBtn");
+    const helpBtn = document.getElementById("helpBtn");
+    const loginBtn = document.getElementById("loginBtn");
+
+    // Function to reset the UI between page changes
+    function resetPage() {
+        const pages = document.querySelectorAll(".page")
+        pages.forEach(function (page) {
+            page.classList.remove("active");
+        });;
+
+        keyboard.style.display = "none";
+
+        backBtn.classList.add("hidden");
+        homeBtn.classList.add("hidden");
+    }
+
+
+    //  Function to load page elements
+    function loadEyeCatcher() {
+        resetPage();
+        eyeCatcherPage.classList.add("active");
+        currentPage = "eye";
+    }
+
+    function loadMainPage() {
+        resetPage();
+        mainPage.classList.add("active");
+        currentPage = "main";
+    }
+
+    function loadSearchPage() {
+        resetPage();
+        searchPage.classList.add("active");
+        backBtn.classList.remove("hidden");
+        keyboard.style.display = "block";
+        currentPage = "search";
+    }
+
+    function loadResultsPage() {
+        resetPage();
+        resultsPage.classList.add("active");
+        backBtn.classList.remove("hidden");
+        homeBtn.classList.remove("hidden");
+        currentPage = "results"
+
+    }
+
+    // Functions for eye catcher page
+    if (eyeCatcherHelpBtn) {
+        eyeCatcherHelpBtn.addEventListener("click", function () {
+            console.log("Eye Catcher Help Btn has been Pressed");
+        });
+    }
+
+    if (eyeCatcherStartBtn) {
+        eyeCatcherStartBtn.addEventListener("click", function () {
+            console.log("Eye Catcher Start Btn has been Pressed");
+            loadMainPage();
+        });
+    }
+
+
+    if (searchBar) {
+        searchBar.addEventListener("click", function () {
+            console.log("Search Bar has been Pressed");
+            loadSearchPage();
+        });
+    }
+
+    if (homeBtn) {
+        homeBtn.addEventListener("click", function () {
+            console.log("Home Btn Has Been Pressed");
+            loadMainPage();
+        });
+    }
+
+
+    // This simulates hitting search so we will now load the result page
+    keyboard.addEventListener("click", function () {
+        console.log("Keyboard has been Pressed, now showing results");
+        loadResultsPage();
+    });
+
+    if (backBtn) {
+        backBtn.addEventListener("click", function () {
+            console.log("Back Button has been Pressed");
+
+            if (currentPage === "search") {
+                loadMainPage();
+
+            } else if (currentPage === "results") {
+                loadSearchPage();
+
+            }
+
+        });
+    }
+
+
+});
